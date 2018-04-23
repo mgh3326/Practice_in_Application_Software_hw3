c8630d73edc7ab550c3424ee06f1d615486bcdf2
mgh3326 <mgh3326@naver.com>
2018-04-23 15:53:51 +0900

    update

--- Server/Form1.Designer.cs
+++ Server/Form1.Designer.cs
@@ -35,10 +35,10 @@
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.MemberList = new System.Windows.Forms.ListView();
-            this.TextBox_ServerLog = new System.Windows.Forms.TextBox();
             this.columnHeaderIndex = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.columnHeaderID = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.columnHeaderPassword = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.TextBox_ServerLog = new System.Windows.Forms.TextBox();
             this.textBox_PortNumber = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
@@ -54,7 +54,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(342, 25);
+            this.label2.Location = new System.Drawing.Point(518, 24);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(49, 15);
             this.label2.TabIndex = 1;
@@ -64,22 +64,25 @@
             // 
             this.textBox_IP.Location = new System.Drawing.Point(49, 22);
             this.textBox_IP.Name = "textBox_IP";
-            this.textBox_IP.Size = new System.Drawing.Size(287, 25);
+            this.textBox_IP.ReadOnly = true;
+            this.textBox_IP.Size = new System.Drawing.Size(446, 25);
             this.textBox_IP.TabIndex = 2;
+            this.textBox_IP.Text = "실행시, 서버 PC의 IP를 자동으로 입력 받아 와서 textBox에 표시";
             // 
             // button_Start
             // 
-            this.button_Start.Location = new System.Drawing.Point(503, 25);
+            this.button_Start.Location = new System.Drawing.Point(679, 25);
             this.button_Start.Name = "button_Start";
             this.button_Start.Size = new System.Drawing.Size(75, 23);
             this.button_Start.TabIndex = 3;
             this.button_Start.Text = "Start";
             this.button_Start.UseVisualStyleBackColor = true;
+            this.button_Start.Click += new System.EventHandler(this.button_Start_Click);
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(13, 67);
+            this.label3.Location = new System.Drawing.Point(28, 67);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(145, 15);
             this.label3.TabIndex = 4;
@@ -88,7 +91,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(278, 67);
+            this.label4.Location = new System.Drawing.Point(362, 67);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(79, 15);
             this.label4.TabIndex = 5;
@@ -102,19 +105,10 @@
             this.columnHeaderPassword});
             this.MemberList.Location = new System.Drawing.Point(31, 85);
             this.MemberList.Name = "MemberList";
-            this.MemberList.Size = new System.Drawing.Size(230, 267);
+            this.MemberList.Size = new System.Drawing.Size(282, 294);
             this.MemberList.TabIndex = 6;
             this.MemberList.UseCompatibleStateImageBehavior = false;
             this.MemberList.View = System.Windows.Forms.View.Details;
-            // 
-            // TextBox_ServerLog
-            // 
-            this.TextBox_ServerLog.Location = new System.Drawing.Point(284, 85);
-            this.TextBox_ServerLog.Multiline = true;
-            this.TextBox_ServerLog.Name = "TextBox_ServerLog";
-            this.TextBox_ServerLog.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.TextBox_ServerLog.Size = new System.Drawing.Size(294, 258);
-            this.TextBox_ServerLog.TabIndex = 7;
             // 
             // columnHeaderIndex
             // 
@@ -129,9 +123,18 @@
             this.columnHeaderPassword.Text = "Password";
             this.columnHeaderPassword.Width = 98;
             // 
+            // TextBox_ServerLog
+            // 
+            this.TextBox_ServerLog.Location = new System.Drawing.Point(354, 85);
+            this.TextBox_ServerLog.Multiline = true;
+            this.TextBox_ServerLog.Name = "TextBox_ServerLog";
+            this.TextBox_ServerLog.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.TextBox_ServerLog.Size = new System.Drawing.Size(400, 294);
+            this.TextBox_ServerLog.TabIndex = 7;
+            // 
             // textBox_PortNumber
             // 
-            this.textBox_PortNumber.Location = new System.Drawing.Point(397, 22);
+            this.textBox_PortNumber.Location = new System.Drawing.Point(573, 21);
             this.textBox_PortNumber.Name = "textBox_PortNumber";
             this.textBox_PortNumber.Size = new System.Drawing.Size(100, 25);
             this.textBox_PortNumber.TabIndex = 8;
@@ -140,7 +143,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 401);
             this.Controls.Add(this.textBox_PortNumber);
             this.Controls.Add(this.TextBox_ServerLog);
             this.Controls.Add(this.MemberList);
--- Server/Form1.cs
+++ Server/Form1.cs
@@ -7,18 +7,103 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
 
 namespace Server
 {
     public partial class Form1 : Form
     {
+
         public Form1()
         {
             InitializeComponent();
-        }
+            //MessageBox.Show(Get_MyIP());
 
+        }
+        public string Get_MyIP()
+        {
+            IPHostEntry host = Dns.GetHostByName(Dns.GetHostName());
+            string myip = host.AddressList[0].ToString();
+            return myip;
+        }
         private void Form1_Load(object sender, EventArgs e)
         {
+            textBox_IP.Text = Get_MyIP();
+
+        }
+
+        private void button_Start_Click(object sender, EventArgs e)
+        {
+            ListViewItem item;
+            if(button_Start.Text == "Start")
+            {
+                if (textBox_IP.Text == "" || textBox_PortNumber.Text == "")
+                {
+                    MessageBox.Show("IP와 Port를 입력하고 Start를 눌러주세요");
+                    return;
+                }
+
+                button_Start.Text = "Stop";
+                button_Start.ForeColor = Color.Red;
+                TextBox_ServerLog.Text = "Server 기다리는 중..";
+                item = MemberList.Items.Add("1");//여기서 파일 받아오면 될것 같다.
+                item.SubItems.Add("root");
+                item.SubItems.Add("password");
+                MessageBox.Show("왜 아무것도 안나오는거야");
+                TcpListener server = null;
+                IPAddress locAddr = IPAddress.Parse(textBox_IP.Text);/* int port = 13000;*/
+                try
+                {
+                    server = new TcpListener(locAddr, Int32.Parse(textBox_PortNumber.Text));
+                    //server = new TcpListener(IPAddress.Parse("127.0.0.1"), 13002);
+                    server.Start();
+                    TextBox_ServerLog.Text += "\nConnected";
+
+                    //listening loop
+                    //while (true)
+                    //{
+                    //    Console.WriteLine("Waiting for a connection...");
+                    //    TcpClient client = server.AcceptTcpClient();
+                    //    MessageBox.Show("여기서 기다릴라나");
+
+                    //    Console.WriteLine("Conneted!");
+
+                    //    DateTime t = DateTime.Now;
+                    //    // string to byte
+                    //    string message = string.Format("서버에서 보내는 메세지 {0}", t.ToString("yyyy-MM-dd hh:mm:ss"));
+                    //    byte[] wrtieBuffer = Encoding.UTF8.GetBytes(message);
+
+                    //    //int to byte
+                    //    int bytes = wrtieBuffer.Length;
+                    //    byte[] wrtieBufferSize = BitConverter.GetBytes(bytes);
+
+                    //    //send to client
+                    //    NetworkStream stream = client.GetStream();
+                    //    //send Buffer
+                    //    stream.Write(wrtieBufferSize, 0, wrtieBufferSize.Length);
+                    //    Console.WriteLine("Sent: {0}", message);
+                    //    stream.Close();
+                    //    client.Close();
+                    //    Console.WriteLine();
+                    //}
+                }
+                catch (SocketException se)
+                {
+                    Console.WriteLine("SocketException:{0}", se);
+                }
+                finally
+                {
+                    server.Stop();
+                }
+                Console.WriteLine("\n서버가 종료됩니다.");
+            }
+            else//Stop일 경우
+            {
+                button_Start.Text = "Start";
+                button_Start.ForeColor = Color.Black;
+            }
+
 
         }
     }
